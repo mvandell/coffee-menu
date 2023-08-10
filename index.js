a234047079a704cab206457409936154822d4238
Marisa <marisavandellos@gmail.com>
2023-08-10 10:21:49 -0700

    added pseudocode and step 4

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const coffeeMenu = require("./coffee_data");
-const drinks = coffeeMenu.map(element => element.name);
+const drinks = coffeeMenu.map(element => element.name); //all drinks
 console.log(drinks);
 
-const cheapDrinks = coffeeMenu.filter(drink => drink.price <= 5);
+const cheapDrinks = coffeeMenu.filter(drink => drink.price <= 5); //drinks for 5 or less
 console.log(cheapDrinks);
+
+const evenDrinks = coffeeMenu.filter(drink => drink.price % 2 === 0); //even priced drinks
+console.log(evenDrinks);
+
+//total of all drinks
+
+//seasonal drinks
+
+//seasonal drinks "with imported beans"
